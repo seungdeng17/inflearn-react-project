e15ec042058e9938b27c23de905c9c6f3e9e07b8
sgdg <seungdeng17@gmail.com>
2021-07-05 23:03:01 +0900

    인증 화면 만들기

--- whois/start/src/App.js
+++ whois/start/src/App.js
@@ -3,6 +3,8 @@ import { Route } from "react-router-dom";
 import "antd/dist/antd.css";
 import Search from "./search/container/Search";
 import User from "./user/container/User";
+import Login from "./auth/container/Login";
+import Signup from "./auth/container/Signup";
 
 export default function App() {
   useEffect(() => {
@@ -15,6 +17,8 @@ export default function App() {
     <>
       <Route exact path="/" component={Search}></Route>
       <Route path="/user/:name" component={User}></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route path="/signup" component={Signup}></Route>
     </>
   );
 }
---
+++ whois/start/src/auth/container/Login.jsx
@@ -0,0 +1,36 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/AuthLayout";
+
+export default function Login() {
+  return (
+    <AuthLayout onFinish={() => {}}>
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: "Please input your Username!" }]}
+      >
+        <Input autoFocus prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "Please input your Password!" }]}
+      >
+        <Input
+          prefix={<LockOutlined />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+          Log in
+        </Button>
+        Or <Link to="/signup">register now!</Link>
+      </Form.Item>
+    </AuthLayout>
+  );
+}
